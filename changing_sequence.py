43ea5b7d76133ec9575b6a9baabd2fdca7c07bbd
theolucky889 <theolucky889@gmail.com>
2024-07-18 18:15:08 +0800

    Update changing_sequence.py

--- changing_sequence.py
+++ changing_sequence.py
@@ -2,10 +2,18 @@ def find_changing_point(arr):
     if len(arr) < 3:
         return -1 # less than 3 elements cannot change trend
 
-    for i in range(1, len(arr)):
-        if arr[i] != arr[i - 1]:
-            trend = arr[i] > arr[i - 1]
-            break
+    # for i in range(1, len(arr)):
+    #     if arr[i] != arr[i - 1]:
+
+    i = 1
+    while i < len(arr) and arr[i] == arr[i - 1]:
+        i += 1
+        
+    if i == len(arr):
+        return -1 # if all equal
+    
+    trend = arr[i] > arr[i - 1]
+    
     for j in range (i, len(arr)):
         if (trend and arr[j] < arr[j - 1]) or (not trend and arr[j] > arr[j - 1]):
             return j - 1
